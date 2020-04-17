a3acb96e29ec3913b2da0de77a444de55d21d0e6
Tim <tim.cieplowski@gmail.com>
2020-04-17 17:22:31 -0400

    update cli tool

--- bin/list-games-with-files.sh
+++ bin/list-games-with-files.sh
@@ -1,1 +1,5 @@
-aws s3 ls s3://audio-bits-data/games/ --recursive --human-readable | grep '\.mp3' | awk '{ split($5, a, "/"); print $1 "\t " a[2] }' | sort | uniq
+aws s3 ls s3://audio-bits-data/games/ --recursive --human-readable \
+ | grep '\.mp3' \
+ | awk '{ split($5, a, "/"); print $1 " - https://tonalrecall.us/app/#/" a[2] }' \
+ | sort \
+ | uniq -c -f1
